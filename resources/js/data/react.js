311224156a3cccdbda86b4f7a44a9d422471e5b0
johnvincentio <john@johnvincent.io>
2017-10-02 11:58:17 -0400

    react

---
+++ resources/js/data/react.js
@@ -0,0 +1,25 @@
+
+/* global APP */
+
+(function() {
+  'use strict';
+
+var data = APP.data.addData("ion-ios-world", "React Resources");
+
+APP.data.addItem(data,
+"react.ico",
+"https://reactjs.org/",
+"React",
+"A Javascript library for building user interfaces."
+);
+APP.model.add(data);
+
+APP.data.addItem(data,
+  "babel.ico",
+  "https://babeljs.io/",
+  "Babel is a JavaScript compiler",
+  "Use next generation JavaScript, today."
+  );
+  APP.model.add(data);
+
+})();
