dcd55165eb70aefe71918267d48b5875678f979c
johnvincentio <john@johnvincent.io>
2017-10-08 12:59:33 -0400

    resources

--- resources/js/data/react.js
+++ resources/js/data/react.js
@@ -23,30 +23,6 @@ APP.data.addItem(data,
 APP.model.add(data);
 
 APP.data.addItem(data,
-  "babel.ico",
-  "https://babeljs.io/",
-  "Babel is a JavaScript compiler",
-  "Use next generation JavaScript, today."
-);
-APP.model.add(data);
-
-APP.data.addItem(data,
-  "react-training.png",
-  "https://reacttraining.com/react-router/web/guides/philosophy",
-  "React Training",
-  "React Training / React Router"
-);
-APP.model.add(data);
-
-APP.data.addItem(data,
-  "jest.ico",
-  "https://facebook.github.io/jest/",
-  "Jest",
-  "Delightful JavaScript Testing"
-);
-APP.model.add(data);
-
-APP.data.addItem(data,
   "react.ico",
   "https://reactjs.org/docs/test-renderer.html",
   "Test Renderer",
@@ -59,6 +35,54 @@ APP.data.addItem(data,
   "https://reactjs.org/docs/shallow-renderer.html",
   "Shallow Renderer",
   "React Testing without a DOM"
+);
+APP.model.add(data);
+
+APP.data.addItem(data,
+  "react-training.png",
+  "https://reacttraining.com/react-router/web/guides/philosophy",
+  "React Training",
+  "React Training / React Router"
+);
+APP.model.add(data);
+
+APP.data.addItem(data,
+  "babel.ico",
+  "https://babeljs.io/",
+  "Babel is a JavaScript compiler",
+  "Use next generation JavaScript, today."
+);
+APP.model.add(data);
+
+APP.data.addItem(data,
+  "jest.ico",
+  "https://facebook.github.io/jest/",
+  "Jest",
+  "Delightful JavaScript Testing"
+);
+APP.model.add(data);
+
+APP.data.addItem(data,
+  "github.png",
+  "https://facebook.github.io/flux/",
+  "Flux",
+  "Application Architecture for Building User Interfaces"
+);
+APP.model.add(data);
+
+APP.data.addItem(data,
+  "redux.ico",
+  "http://redux.js.org/",
+  "Redux",
+  "Redux is a predictable state container for JavaScript apps"
+);
+APP.model.add(data);
+
+APP.data.addItem(data,
+  "npmjs.png",
+  "https://www.npmjs.com/package/redux-thunk",
+  "Redux-Thunk",
+  "Thunk middleware for Redux"
 );
 APP.model.add(data);
 
