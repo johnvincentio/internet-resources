b61c3a72eae557e2276bc8df0fddeb4238b09701
johnvincentio <john@johnvincent.io>
2018-03-21 14:09:45 -0400

    more resources

--- resources/js/data/colors-tools.js
+++ resources/js/data/colors-tools.js
@@ -25,8 +25,16 @@ APP.model.add(data);
 APP.data.addItem(data,
 "flatuicolors.png",
 "http://flatuicolors.com/",
-"Flat UI Colors",
-"A good starting point for choosing a flat design color for your next project."
+"Flat UI Colors 2",
+"A good starting point for choosing a flat design color for your next project"
+);
+APP.model.add(data);
+
+APP.data.addItem(data,
+"flatiucolors.ico",
+"https://www.flatuicolors.net/",
+"FlatUIColors.net",
+"FlatUIColors is a color palette that emerges from the current concept of flat design"
 );
 APP.model.add(data);
 
@@ -34,7 +42,23 @@ APP.data.addItem(data,
 "materialpalette.jpg",
 "http://www.materialpalette.com/",
 "Material Palette",
-"Another great set of colors, inspired in material design. Generate and download your palette."
+"Another great set of colors, inspired in material design. Generate and download your palette"
+);
+APP.model.add(data);
+
+APP.data.addItem(data,
+"color-hex.ico",
+"http://www.color-hex.com/",
+"Color Hex Color Codes",
+"Color-hex gives extensive information about colors"
+);
+APP.model.add(data);
+
+APP.data.addItem(data,
+"beautycolorcode.ico",
+"https://www.beautycolorcode.com/",
+"About Color Hex",
+"Color Hex is a free color tool providing information about any color"
 );
 APP.model.add(data);
 
@@ -42,7 +66,7 @@ APP.data.addItem(data,
 "colorhunt.gif",
 "http://www.colorhunt.co/",
 "Colorhunt Palettes",
-"Don't know what colors to use for your website? Colorhunt helps you with beautiful color palettes."
+"Don't know what colors to use for your website? Colorhunt helps you with beautiful color palettes"
 );
 APP.model.add(data);
 
@@ -50,7 +74,7 @@ APP.data.addItem(data,
 "lolcolors.svg",
 "http://www.lolcolors.com/",
 "LOL Colors Palettes",
-"Curated color palettes inspiration. Another source for great color palettes."
+"Curated color palettes inspiration. Another source for great color palettes"
 );
 APP.model.add(data);
 
@@ -58,7 +82,7 @@ APP.data.addItem(data,
 "uigradients.png",
 "http://uigradients.com",
 "UI Gradients",
-"Collection of beautiful color gradients for you to choose from and export to your project."
+"Collection of beautiful color gradients for you to choose from and export to your project"
 );
 APP.model.add(data);
 
@@ -66,7 +90,7 @@ APP.data.addItem(data,
 "paletton.png",
 "http://paletton.com",
 "Paletton",
-"A very popular tool which allows you to create color combinations that play well together."
+"A very popular tool which allows you to create color combinations that play well together"
 );
 APP.model.add(data);
 
@@ -74,7 +98,7 @@ APP.data.addItem(data,
 "0to255.png",
 "http://www.0to255.com/",
 "0to255",
-"A simple tool that helps you find variations of any color. Perfect for hovers, borders and gradients."
+"A simple tool that helps you find variations of any color. Perfect for hovers, borders and gradients"
 );
 APP.model.add(data);
 
